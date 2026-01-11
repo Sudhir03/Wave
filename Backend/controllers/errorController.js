6196bbd252fe9f8dc6d86d9811f259677e2885f8
Sudhir Sharma <sudhirsharma9018@gmail.com>
2026-01-12 00:27:30 +0530

    log

--- Backend/controllers/errorController.js
+++ Backend/controllers/errorController.js
@@ -1,13 +1,13 @@
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
 
-  if (process.env.NODE_ENV === "development") {
-    return res.status(err.statusCode).json({
-      isSuccess: false,
-      message: err.message,
-      stack: err.stack,
-    });
-  }
+  // if (process.env.NODE_ENV === "development") {
+  return res.status(err.statusCode).json({
+    isSuccess: false,
+    message: err.message,
+    stack: err.stack,
+  });
+  // }
 
   if (process.env.NODE_ENV === "production") {
     // Send only operational errors to client
